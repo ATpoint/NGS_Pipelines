36772c0427e8168e7010c7aa43c56ff1fbc284d7
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2021-07-09 10:34:58 +0200

    Update cleanup_v1.0.0.sh

--- cleanup_v1.0.0.sh
+++ cleanup_v1.0.0.sh
@@ -12,18 +12,22 @@ function exist_move {
 
 rm *.fastq.gz
 
-exist_move "*raw.bam*" "bam_raw"; exist_move "*raw.flag*" "./bam_raw"
+exist_move "*_raw.bam*" "./bam_raw"
 
-exist_move "*_dedup.bam*" "./bam_dedup"; exist_move "*_dedup.flag*" "./bam_dedup"
+exist_move "*_raw.flagstat" "./bam_raw"
+
+exist_move "*_dedup.bam*" "./bam_dedup"
+
+exist_move "*_dedup.flag*" "./bam_dedup"
 
 exist_move "*.bigwig" "./bigwig"
 
 exist_move "*fastqc.*" "./fastqc"
 
-exist_move "*peaks*" "./macs2"
+exist_move "*_peaks*" "./macs2"
 
 exist_move "*.log" "./logs"
 
-exist_move "*InsertSize*" "./insert_sizes"
+exist_move "*_InsertSize*" "./insert_sizes"
 
-exist_move "*cutsites.bed.gz" "./bed_cutsites"
+exist_move "*_cutsites.bed.gz" "./bed_cutsites"
